742f10ac3934a63d7cab4519b05626d8b43c2ed0
Urs Mayr <mayr.urs@gmail.com>
2025-04-09 09:24:13 +0200

    Init commit

---
+++ lua/plugins/codecompanion.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "git@github.com:olimorris/codecompanion.nvim.git", -- Use SSH instead of HTTPS
+    lazy = false, -- Ensures the plugin loads at startup
+    config = function()
+      require("codecompanion").setup({
+        strategies = {
+          chat = {
+            adapter = "openai",
+          },
+          inline = {
+            adapter = "anthropic",
+          },
+          cmd = {
+            adapter = "anthropic",
+          },
+        },
+        adapters = {
+          openai = function()
+            return require("codecompanion.adapters").extend("openai", {
+              env = {
+                api_key = os.getenv("OPENAI_KEY"),
+              },
+            })
+          end,
+          anthropic = function()
+            return require("codecompanion.adapters").extend("anthropic", {
+              env = {
+                api_key = os.getenv("ANTHROPIC_KEY"),
+              },
+            })
+          end,
+        },
+      })
+    end,
+    keys = {
+      { "<leader>ci", ":CodeCompanion<CR>", desc = "CodeCompanion" },
+      { "<leader>cc", ":CodeCompanionChat<CR>", desc = "CodeCompanion Chat" },
+      { "<leader>ct", ":CodeCompanionToggle<CR>", desc = "CodeCompanion Chat" },
+    },
+  },
+}
